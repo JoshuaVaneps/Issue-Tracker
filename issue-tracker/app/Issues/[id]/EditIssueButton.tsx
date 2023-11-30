ddc447ddd860bb68453cd4cf6b6e09320b33f8fa
Joshua Vaneps <joshuavaneps@yahoo.com>
2023-11-30 15:11:08 -0500

    added delete button

--- issue-tracker/app/Issues/[id]/EditIssueButton.tsx
+++ issue-tracker/app/Issues/[id]/EditIssueButton.tsx
@@ -1,13 +1,12 @@
 import { Pencil2Icon } from "@radix-ui/react-icons";
-import { Button } from "@radix-ui/themes";
+import { Box, Button } from "@radix-ui/themes";
 import Link from "next/link";
 
 export const EditIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Link href={`/issues/${issueId}/edit`}>
-      <Button>
-        Edit Issue <Pencil2Icon />
-      </Button>
-    </Link>
+    <Button>
+      <Pencil2Icon />
+      <Link href={`/issues/${issueId}/edit`}>Edit Issue</Link>
+    </Button>
   );
 };
--- issue-tracker/app/Issues/[id]/IssueDetails.tsx
+++ issue-tracker/app/Issues/[id]/IssueDetails.tsx
@@ -7,11 +7,11 @@ const IssueDetails = ({ issue }: { issue: Issue }) => {
   return (
     <>
       <Heading>{issue.title}</Heading>
-      <Flex gap="3" my="2">
+      <Flex className="space-x-3" my="2">
         <IssuesStatusBadge status={issue.status} />
         <Text>{issue.created_at.toDateString()}</Text>
       </Flex>
-      <Card className="prose" mt="4">
+      <Card className="prose max-w-full" mt="4">
         <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
     </>
